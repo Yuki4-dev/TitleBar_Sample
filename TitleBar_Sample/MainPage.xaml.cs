83ebe488907627ac3bed938d40558e516ab44a6a
okamotoYukityoshi <sanakan222@gmail.com>
2020-10-30 23:37:28 +0900

    プロジェクト ファイルを追加します。

---
+++ TitleBar_Sample/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace TitleBar_Sample
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationView.GetForCurrentView().TitleBar.ButtonBackgroundColor = Windows.UI.Colors.Transparent;
+            ApplicationView.GetForCurrentView().TitleBar.ButtonInactiveBackgroundColor = Windows.UI.Colors.Transparent;
+            Window.Current.SetTitleBar(TitleBar);
+
+            CoreApplication.GetCurrentView().TitleBar.LayoutMetricsChanged += TitleBar_LayoutMetricsChanged;
+        }
+
+        private void TitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            RightText.Margin = new Thickness(0, 0, sender.SystemOverlayRightInset, 0);
+        }
+    }
+}
+
